949200383f9a6257d2bd060a6cf3150f914de46d
Xiaoqin Gu <48537852+xgu89@users.noreply.github.com>
2022-11-03 11:52:12 +0800

    Revert "feat:development (#1)"
    This reverts commit 4397ccc79af4d859d9d573bdfc8b2a5f2d135210.

--- src/health-check/health-check.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HealthCheckController } from './health-check.controller';
-import { ConfigModule } from '@nestjs/config';
-
-@Module({
-  imports: [ConfigModule],
-  controllers: [HealthCheckController],
-})
-export class HealthCheckModule {}
